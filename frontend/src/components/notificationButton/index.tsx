01d2132a058dfbfe1213896f6270888fe16076eb
Lucas Marinho <lucascm00@gmail.com>
2022-11-20 11:18:47 -0300

    Send notification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,9 +1,22 @@
-import icon from '../../assets/imgs/notification-icon.svg'
-import './style.css'
+import axios from 'axios';
+import icon from '../../assets/imgs/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+import './style.css';
 
-function NotificationButton() {
+type props = {
+    saleId: number;
+}
+
+function handleClick(id:number){
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log('sucesso');
+    })
+}
+
+function NotificationButton({saleId} : props) {
     return (
-        <div className = "dsmeta-red-btn">
+        <div className = "dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src= {icon} alt="Notificar" />
         </div>
     )
